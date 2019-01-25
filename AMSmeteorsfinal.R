592af93dc245d92b4de95d9b0c6b0999ad9f764b
Jpvanreckless <juan.lorcag@utem.cl>
2019-01-25 03:12:21 -0300

    Se corrigen errores y se agrega extracción de USUARIOS

--- AMSmeteorsfinal.R
+++ AMSmeteorsfinal.R
@@ -1,14 +1,13 @@
-#==================== AMS.METEORS.ORG ====================#
+##==================== AMS.METEORS.ORG ====================#
 
 ##########################################################
-##### Iniciando la extracción de información general #####
+##### Iniciando la extracci?n de informaci?n general #####
 ##########################################################
 library(rvest)
 
-# Se asigna la página de ams.meteors.org/firelogs
+# Se asigna la p?gina de ams.meteors.org/firelogs
 webpage<- 'https://www.amsmeteors.org/about/'
 
-#Leyendo página
 webpage <- read_html(webpage)
 
 # Extrayendo contenido en la clase About AMS
@@ -36,43 +35,90 @@ print(textoNoticia)
 # Separando las palabras por espacio
 splitEspacioNoticia <- strsplit(textoNoticia," ")[[1]]
 
-# Pasando todas las palabras a minúsculas
+# Pasando todas las palabras a min?sculas
 splitEspacioNoticia <- tolower(splitEspacioNoticia)
 
 # Contando palabras
 unlistNoticias<-unlist(splitEspacioNoticia)
 tablaPalabras<-table(unlistNoticias)
 
-# Pasando la información a un data frame
+# Pasando la informaci?n a un data frame
 dfPalabrasNoticia <- as.data.frame(tablaPalabras)
 
-# Almacenando la información en CSV
+# Almacenando la informaci?n en CSV
 write.csv(dfPalabrasNoticia, file="PalabrasNoticia.csv")
 
-# Almacenando la información en un txt
+# o en un txt
 write.table(dfPalabrasNoticia, file="PalabrasNoticia.txt")
 
 ##########################################################
-######## Iniciando la extracción de avistamientos ########
+######## Iniciando la extracci?n de avistamientos ########
 ##########################################################
 
-# Se asigna la página de ams.meteors.org/observations/
-Fireball<- 'https://www.amsmeteors.org/observations/'
+fullTable <- data.frame()
 
-#Leyendo página
-Fireball <- read_html(Fireball)
+for (i in 1:3) {
+  # Se asigna la p?gina de ams.meteors.org/firelogs
+  Fireball <- paste("https://www.amsmeteors.org/members/imo_view/browse_events?country=-1&year=2018&num_report=-99&page=",i,sep = "")
+  
+  print(Fireball)
+  
+  Fireball <- read_html(Fireball)
+  
+  # Extrayendo contenido en la clase registro de avistamientos
+  tablainfo <- html_nodes(Fireball,"table")
+  
+  # Extraccion informacion tabla 
+  tabla1 <- html_table(tablainfo,fill = TRUE)
+  
+  # Convierte a data.frame
+  dfTabla <- data.frame(tabla1)
+  
+  # Elimina datos malos
+  dfTabla <- dfTabla[-c(1), ]
+  dfTabla$NA.<- NULL
+  dfTabla$NA..1 <- NULL
+  dfTabla$NA..2 <- NULL
+  
+  # Viendo a priori la info en la variable textoNoticia
+  print(dfTabla)
+  
+  fullTable <- rbind(fullTable,dfTabla)
+  
+  # Eliminando elementos repetidos
+  fullTable <- unique(fullTable)
+  
+}
+
+#AGREGAR GRAFICOS Y OTRAS COSAS
+
+
+##########################################################
+######## Iniciando la extracci?n de usuarios #############
+##########################################################
+
+fullTable2 <- data.frame()
+
+for (i in 1:7) {
+  # Se asigna la p?gina de ams.meteors.org/observations/
+  Users <- paste("https://www.amsmeteors.org/observations/",i,sep = "")
+  
+#Leyendo p?gina
+Users <- read_html(Users)
 
 # Extrayendo contenido en la Observaciones Table Responsive
-tabla <- html_nodes(Fireball,".table-responsive")
+tabla <- html_nodes(Users,".table-responsive")
 
 # Extrayendo contenido en la clase tabla respuestas
 tablainfo <- html_nodes(tabla,"table")
 
 
-
-# Extraccion informacion tabla 
+# Extracci?n informacion tabla 
 tabla1 <- html_table(tablainfo)
 
-# Viendo a priori la info en la variable textoNoticia
+# Viendo a priori la info en la variable tabla1
 print(tabla1)
 
+}
+
+#JUNTAR EN 1 GRAFICO Y AGREGAR MAS COSAS 
